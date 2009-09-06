421d11da61d7c052260cc2187c3c0ea5728099b8
Kazutaka Miyasaka <kazmiya@gmail.com>
2009-09-06 19:48:21 +0900

    better term handling

--- action.php
+++ action.php
@@ -20,7 +20,7 @@ class action_plugin_asiansearch extends DokuWiki_Action_Plugin {
         return array(
             'author' => 'Kazutaka Miyasaka',
             'email'  => 'kazmiya@gmail.com',
-            'date'   => '2009-09-03',
+            'date'   => '2009-09-06',
             'name'   => 'Asian Search Plugin',
             'desc'   => 'Manipulates a search query for a better search experience in Asian languages',
             'url'    => 'http://www.dokuwiki.org/plugin:asiansearch'
@@ -40,14 +40,9 @@ class action_plugin_asiansearch extends DokuWiki_Action_Plugin {
     function handleQuery(&$event, $param) {
         $data =& $event->data;
 
-        var_dump(ft_queryParser($data['query']));
-
         // manipulate a query
         $terms = preg_split('/(".*?")/u', $data['query'], -1, PREG_SPLIT_DELIM_CAPTURE);
         $data['query'] = implode('', array_map(array($this, '_manipulateTerm'), $terms));
-
-        var_dump(ft_queryParser($data['query']));
-        //msg(var_dump(_ft_pageSearch($data)));
     }
 
     /**
@@ -62,9 +57,6 @@ class action_plugin_asiansearch extends DokuWiki_Action_Plugin {
             // treat ideographic spaces (U+3000) as search term separators
             $str = preg_replace('/\x{3000}/u', ' ',  $str);
 
-            // tokenize a term by means of morphological analyzer
-            $str = $this->_tokenize($str);
-
             // make phrases for asian characters
             $str = implode(' ', array_map(array($this, '_makePhrase'), explode(' ', $str)));
         }
@@ -78,40 +70,8 @@ class action_plugin_asiansearch extends DokuWiki_Action_Plugin {
         // skip if $str has a search modifier
         if (! preg_match('/^[\-\@\^]/u', $str)) {
             $str = preg_replace('/('.IDX_ASIAN.'+)/u', ' "$1" ', $str);
-            //$str = preg_replace('/ {2,}/', ' ', $str);
             $str = trim($str);
         }
         return $str;
-    }
-
-    function _tokenize($str = '') {
-        // thanks to Osamu Higuchi: DokuWikiの日本語対応 (Japanese)
-        // http://www.higuchi.com/dokuwiki/dokuwiki:localize
-
-        if (! function_exists(proc_open)) { return $str; }
-
-        $cmd_tokenize = '/usr/bin/mecab -O wakati';
-        $dspec = array(
-            0 => array('pipe', 'r'),
-            1 => array('pipe', 'w'),
-            2 => array('file', '/dev/null', 'w')
-        );
-        $process = proc_open($cmd_tokenize, $dspec, $pipes);
-
-        if (! is_resource($process)) { return $str; }
-
-        stream_set_blocking($pipes[0], FALSE);
-        stream_set_blocking($pipes[1], FALSE);
-        fwrite($pipes[0], $str."\n");
-        fclose($pipes[0]);
-
-        $str_processed = '';
-        while (! feof($pipes[1])) {
-            $str_processed .= fgets($pipes[1], 32768);
-        }
-        fclose($pipes[1]);
-        proc_close($process);
-
-        return $str_processed;
     }
 }
